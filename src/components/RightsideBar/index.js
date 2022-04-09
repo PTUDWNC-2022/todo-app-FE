3d5524886a8016c076fe98b27338ef5666007264
duy_n_a <nguyenanhduy0400@gmail.com>
2022-04-09 16:51:13 +0700

    Duy | UI for Right panel

--- src/components/RightsideBar/index.js
+++ src/components/RightsideBar/index.js
@@ -1,66 +1,114 @@
-import './RightsideBar.css';
-import { Card, Col, Form, Row } from 'react-bootstrap';
-import React, { useEffect, useRef, useState } from 'react';
+import "./RightsideBar.css";
+import { Card, Col, Form, Row } from "react-bootstrap";
+import React, { useEffect, useRef, useState } from "react";
+import Datetime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
 
 const RightSideBar = ({ todoDetail }) => {
-	const [expand, setExpand] = useState(false);
+  const [expand, setExpand] = useState(false);
+  const [todoName, setTodoName] = useState("");
 
-	function useOutsideAlerter(ref) {
-		useEffect(() => {
-			/**
-			 * If clicked on outside of element
-			 */
-			function handleClickOutside(event) {
-				if (
-					ref.current &&
-					!ref.current.contains(event.target) &&
-					!event.target.classList.contains('todo-item') &&
-					!event.target.classList.contains('form-check-label') &&
-					!event.target.classList.contains('form-check-input')
-				) {
-					setExpand(false);
-				} else {
-					setExpand(true);
-				}
-			}
-			// Bind the event listener
-			document.addEventListener('mousedown', handleClickOutside);
-			return () => {
-				// Unbind the event listener on clean up
-				document.removeEventListener('mousedown', handleClickOutside);
-			};
-		}, [ref]);
-	}
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      // Update current todo
+      setTodoName(todoDetail.name);
 
-	const containerRef = useRef(null);
-	useOutsideAlerter(containerRef);
+      /**
+       * If clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (
+          ref.current &&
+          !ref.current.contains(event.target) &&
+          !event.target.classList.contains("todo-item") &&
+          !event.target.classList.contains("form-check-label") &&
+          !event.target.classList.contains("form-check-input")
+        ) {
+          setExpand(false);
+        } else {
+          setExpand(true);
+        }
+      }
 
-	return (
-		<div
-			className={`container w-25 ${!expand ? 'collapse-container' : ''}`}
-			ref={containerRef}>
-			<Card body>
-				<Row>
-					<Col xl={1} className="pt-1">
-						<Form.Check
-							type="checkbox"
-							checked={todoDetail.isCompleted}
-							onChange={() => {}}
-						/>
-					</Col>
-					<Col xl={11}>
-						<span
-							role="textbox"
-							dangerouslySetInnerHTML={{ __html: todoDetail.name }}
-							contentEditable
-							type="text"
-							className="todo-name"
-						/>
-					</Col>
-				</Row>
-			</Card>
-		</div>
-	);
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref, todoDetail]);
+  }
+
+  const containerRef = useRef(null);
+  useOutsideAlerter(containerRef);
+
+  const onFocusOut = (event) => {
+    setTodoName(event.currentTarget.textContent);
+  };
+
+  return (
+    <div
+      className={`container w-25 ${!expand ? "collapse-container" : ""}`}
+      ref={containerRef}
+    >
+      <Card body>
+        <Row>
+          <Col xl={1} className="d-flex align-items-center">
+            <Form.Check
+              type="checkbox"
+              checked={todoDetail.isCompleted}
+              onChange={() => {}}
+            />
+          </Col>
+          <Col xl={11}>
+            <span
+              role="textbox"
+              dangerouslySetInnerHTML={{
+                __html: todoName ? todoName.toString() : "",
+              }}
+              contentEditable
+              type="text"
+              className="todo-name"
+              onBlur={onFocusOut}
+            />
+          </Col>
+        </Row>
+      </Card>
+      <Card className="my-2 due-date-card">
+        <Card.Body className="">
+          <Row>
+            <Col xl={1} className="d-flex align-items-center">
+              <div>
+                <i className="bi bi-calendar-check" />
+              </div>
+            </Col>
+            <Col xl={10}>
+              <Datetime
+                inputProps={{
+                  placeholder: "Add due date",
+                  style: {
+                    border: "none",
+                    outline: "none",
+                    boxShadow: "none",
+                    color: "rgb(13, 110, 253)",
+                  },
+                }}
+              />
+            </Col>
+            <Col xl={1} className="close-button d-none">
+              <i className="bi bi-x" />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <p>Add note</p>
+          <textarea className="note"></textarea>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default RightSideBar;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Nav } from 'react-bootstrap';
+import React from "react";
+import { Nav } from "react-bootstrap";
 
-import './Sidebar.css';
+import "./Sidebar.css";
 
 const Sidebar = () => {
-	return (
-		<div className="sidebar">
-			<div className="sidebar-header">
-				<i class="bi bi-list icon"></i>
-			</div>
-			<Nav variant="tabs" className="flex-column">
-				<Nav.Item>
-					<Nav.Link href="/home">Active</Nav.Link>
-				</Nav.Item>
-				<Nav.Item>
-					<Nav.Link eventKey="link-1">Longer NavLink</Nav.Link>
-				</Nav.Item>
-				<Nav.Item>
-					<Nav.Link eventKey="link-2">Link</Nav.Link>
-				</Nav.Item>
-			</Nav>
-		</div>
-	);
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <i className="bi bi-list icon" />
+      </div>
+      <Nav variant="tabs" className="flex-column">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Longer NavLink</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
+  );
 };
 export default Sidebar;
